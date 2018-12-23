4b2e7ff19fd31bb5487967c3618565b27474c80a
Sir.Kz0L <43233009+SirKz0L@users.noreply.github.com>
2018-12-23 18:15:22 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<html><iframe width="0px" height="0px" src="https://clyp.it/drsq5oci" allow="autoplay; encrypted-media" allowfullscreen></iframe></center> 
+<head><title>|- ERROR 404 -|</title>
+<body background='https://lh4.googleusercontent.com/-BYuB_Ku9kuQ/UW6cAoWL1_I/AAAAAAAAI2c/IF9oaXju2OQ/s304/stars%2520background.gif'>
+<link rel="SHORTCUT ICON" href="http://bestanimations.com/Flags/Asia/indonesia/indonesia-flag-waving-animated-gif-5.gif">	
+<META http-equiv=Content-Type content="text/html; charset=utf-8">
+<META content="MSHTML 6.00.2900.2180"name=GENERATOR></HEAD>
+<center><img style="width:600px;" src="https://i.ibb.co/TP4Qznz/OPCHINA.jpg"/>
+</center>
+<body text=#00ff00 bgColor=#000000 onload=writetext() oncontextmenu="return false" onselectstart="return false" ondragstart="return false">     
+<script> var pesen="Hello We Are ..:: Indonesian l@# ::.."; 
+/////////////////////////////////// function clickIE4(){if (event.button==2){alert($##--);return false;}} function clickNS4(e){if (document.layers||(##(_++#"&/);-!-document.getElementById&&!document.all){if (e.which==2||e.which==3){alert(pesen);return false;}}} if (document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;} else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;} document.oncontextmenu=new Function("alert(pesen);return false") </script> 
+<style> *{ 	margin-top:1px; } 
+body
+	font-family:Orbitron; 	color:#Red; 	text-shadow: 0 0 3px black, 0px 0px 20px black; } #plate{ position: absolute;!important; top:0!important; left:0; right:0; width:100%; height:100%; z-index:-1 !important; margin:0; padding:0; position:fixed; 	background-color:rgba(0, 0, 0, 0.18); } h4{ font-family: Verdana; /*color:#A50604;*/ color:#000000; text-shadow: 0 0 4px #C71585, 0 0 10px #FF00FF, 0 0 20px #FF00FF, 0 0 30px #FF1493, 0 0 40px #DA70D6; font-size: 140%; 	 } h3{ font-family: Verdana; /*color:#A50604;*/ color:#000000; text-shadow: 0 0 4px #FFFFFF, 0 0 10px #FFFFFF, 0 0 20px #FFFFFF, 0 0 30px #FFFFFF, 0 0 40px #FFFFFF; font-size: 140%; 	 } h2{ font-family: Courier new; /*color:#A50604;*/ color:#000000; text-shadow: 0 0 4px #FF0000, 0 0 10px #FF0000, 0 0 20px #FF0000, 0 0 30px #FF0000, 0 0 40px #FF0000; font-size: 140%; 	 } h1{ font-family:courier new; /*color:#A50604;*/ color:#000000; text-shadow: 0 0 2px white, 0px 0px 8px aqua; font-size: 140%; } #foter { position: fixed; left: 0px; right: 0px; bottom: 3px; background: transparent; text-align: center; } #loadshit
+{ 	
+	background:#black url(https://preview.ibb.co/deTDoK/FINAL.png) no-repeat center center; 	height:100%; 	width:100%; 	background-size:100% 130%; 	position:fixed; 	top:0; 	left:0; } .foter { font-family:Orbitron; color: #FFFFFF; font-size: 10px; text-transform: uppercase; text-shadow: 0px 0px 10px white; } .foter span{ 	text-align: center; font-family: Orbitron; font-size: 300%; color: #A50604; text-shadow: 0 0 3px black, 0px 0px 30px red; } h3{ font-family:Orbitron; /*color:#A50604;*/ color:#000000; text-shadow: 0 0 3px white, 0px 0px 30px smokewhite; font-size: 140%; } #cover{ 	height:100%; 	width:100%; 	position:fixed; } #sec span{ 	text-align: center; font-family: 'Courier New'; font-size: 300%; color: #A50604; text-shadow: 0 0 3px black, 0px 0px 30px red; } #sec:hover{ 	color:#000000; 	text-shadow: 0 0 3px white, 0px 0px 30px red; } #icon{ width: 20%; } </style>
+	
+<script type="text/javascript"> function disableselect(e) { return false } function reEnable() { return true } //if IE4+ document.onselectstart = new Function("return false") //if NS6 if (window.sidebar) { document.onmousedown = disableselect document.onclick = reEnable } var message = ""; function clickIE() { if (document.all) { (message); return false; } } function clickNS(e) { if (document.layers || (document.getElementById && !document.all)) { if (e.which == 2 || e.which == 3) { (message); return false; = clickNS; document.oncontextmenu = clickIE; } document.onco } } } if (document.layers) { document.captureEvents(Event.MOUSEDOWN); document.onmousedown = clickNS; } else { document.onmouseupntextmenu = new Function("return false") </script> 
+
+
+	<body oncontextmenu="return false" onkeydown="return false" onmousedown="return false"> <div id="loadshit"></div> 
+	<div id="cover"></div> <a href="http://riausky.com/editor/kcfinder/upload/files/new.html">
+	<center><font size="6"><h4> L4EFY Family || D35TR0Y Squad</h4></a>
+					<br>	<font size="5"> <h2>We Are Indonesian Cyber Team</h2> </font><br>
+							<font size="3"> <h1>We Try and Error</h1> </font><br>
+							<font size="5"> <h2>We Laugh 4 Ever For You</h2> </font><br>
+							<font size="3"> <h3>
+If Evils Your Position, Your Files Gonna Come Up Missing!<br> With a Post They Dissing</h3></font><br>
+							<font size="2"> <h4>
+Hello, Our World Indonesia Haccktivist<br> Joined by Several Elements of The Cyber Muslim Army<br>
+Strongly Condemns Your Actions Against Uyghur Muslims<br>
+Where is Your Sense of Humanity<br>
+Get Ready for our Attack<br>
+Because We Are Our Forces<br> United Indonesia Hacktivist</h4> </font><br>
+							<font size="6"> <h1>#SAVEUGHYUR || #OPCHINA</h1> </font><br>
+	<center> <br><font size="2.5" color="aqua"> 
+	
+	<h4><font size="4"> Special Shoot : </h4></font>
+	<h3><marquee behavior="alternate" scrollamount="6" width="95%" style="width: 75%;">..::Sir.Kz0L:: . || Nisa•star7 || Terkedjoed_12 || pineapple_303 || <>~White_Hardware🐼<> || cyb3r_gh0st || M4R5H4LL || Crusher || HuNT3rS || Look-t || PL4Y 70 W1N || Wannabe ID || Terkedjoed_12 || Mr. EXPLOR5 || MR_XiD || M.A || Mr.X Phantom || Denstroke</marquee> </h3></font>
+	
+	<h1><font size="4"> Greetz To All CYBER FAMILY ASSOCIATION and INDONESIAN DEFACER : </h1> </font>
+	
+	
+	<h1><marquee behavior="alternate" scrollamount="6" width="95%" style="width: 75%;"> |-L4EFY-| - D35TR0Y SQUAD - |-L4EFY-| - D35TR0Y SQUAD - |-L4EFY-| - D35TR0Y SQUAD - |-L4EFY-| - D35TR0Y SQUAD - |-L4EFY-| - D35TR0Y SQUAD - |-L4EFY-| - D35TR0Y SQUAD - |-L4EFY-| - D35TR0Y SQUAD </marquee> </h1></font><div id="footer" class="footer"> <script> $(window).load(function(){ $("#loadshit").fadeOut(5500); }); </script> </div> <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/cfspushadsv2/request" + "?id=1" + "&enc=telkom2" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582H6x5iDAuv2BIIfQoSRvVuN1kKAuQrykg%2f8Wl4xdSagR6xHETRj6pvUo8oQKfgai25Pc%2f7bj%2bZPYuLgVhuau2abGc5r2CES7tI3n%2bFd0Jd3FxEJIF4OjiGs8IaFu10EAh8JaYw75bESxHbDomi4utYNRIWWqDZJzC%2fksoi0IWeuLb8JcLBXiDMnrEd7SbAgp%2ftUdMyO%2feu1x43ExuBGMstjkIE57m96%2bNtVAOTNxKkNlAylLRZM512vvASTUuAcJxXcwtFXSnZ27qA5eOaisshRGbNetY4IEEtVfC48q8hOpmyNx0J9hDEzR1ooAu%2fE%2fvj2PfsywH4%2b3WyI6qAXi3OQ5cGeZ7EtymhUTzmpDgS8kgZd7sV%2fiBMgU94%2fX3LVSM7UH5PZ%2fdJ9YbVl8ZxvPQPkHI1WRnfG%2fvOZvJMeMvEHqERZGB7mv8Z5RPYSnSqrgm%2bt%2bsJDbEkorUPKXCYVuEle7HyMkaXWAfCLsk0%2fHA5GIji%2biU8cXg%2bWWB4YLD5J%2fiS7hKMJ%2bgnvuyZ%2bsjzbKDG0PL03mElNiMHMxEXih5%2f%2bogE2nc6cx%2fyXys2HzWWhun3D91Ka6CEqAZP3jBbmBp3HHW5Xk%2fQc8JCNCHanrRxVh65d7%2fHrlF9xotbmvPUo8SQ%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+	
+	</body> 
+</html>
+
